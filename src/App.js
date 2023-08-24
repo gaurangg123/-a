1d56ff9c0fb499dcc078ad6471d558c10a36a870
Gaurang Ashava <gaurangashava@gmail.com>
2023-08-25 00:02:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import {useState, usestate} from 'react';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 import Axios from 'axios';
+function App() {
+  const [data, setData] = useState([])
+  const getData = () => {
+    Axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=de76e7cdc7f746d898982e7782964c2d&q=india")
+    .then((response) => {
+      console.log(response)
+      setData(response.data.articles)
+    })
+  }
+  return (
+    <>
+    <div>
+      <button className='btn btn-primary' onClick={getData}>Get News</button>
+    </div>
+    <div className='container'>
+      <div className='row'>
+        { data.map((value) => {
+          return(
+            <div className="card" style={{width:"18rem"}}>
+        <img src={value.urlToImage} className="card-img-top" alt="..."/>
+        <div className="card-body">
+        <h5 className="card-title">{value.title}</h5>
+        <p className="card-text">{value.description}</p>
+        <a href={value.url}> <button className="btn btn-primary" >Go somewhere</button></a>
+        </div>
+    </div>
+          )
+        })
 
-function App(){
-    const[add, setAdd] = useState([])
-    
-}
+        }
+      </div>
+    </div>
+    </>
+  );
+}   
+export default App;
+ 
